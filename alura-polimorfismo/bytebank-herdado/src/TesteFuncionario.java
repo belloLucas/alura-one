d511dbcdca068aadace3df8b0748227ef8b36192
belloLucas <lucasbellodev@gmail.com>
2023-07-10 15:03:37 -0300

    Curso de polimorfismo e herança iniciado

---
+++ alura-polimorfismo/bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,13 @@
+public class TesteFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Lucas Bello");
+        funcionario.setCpf("123.456.789-10");
+        funcionario.setSalario(1345.50);
+
+        System.out.println(funcionario.getNome());
+        System.out.println(funcionario.getCpf());
+        System.out.println(funcionario.getSalario());
+        System.out.println(funcionario.getBonificacao());
+    }
+}
