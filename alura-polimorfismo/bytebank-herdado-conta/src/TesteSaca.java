513964a06d7ea3e207bc0e0f48e7034fa2718492
belloLucas <lucasbellodev@gmail.com>
2023-07-24 08:47:21 -0300

    testando exceptions checked

--- alura-polimorfismo/bytebank-herdado-conta/src/TesteSaca.java
+++ alura-polimorfismo/bytebank-herdado-conta/src/TesteSaca.java
@@ -2,7 +2,12 @@ public class TesteSaca {
     public static void main(String[] args) {
         Conta conta = new ContaCorrente(123, 321);
         conta.deposita(200.0);
-        conta.saca(190.0);
+
+        try {
+            conta.saca(210.0);
+        } catch(Exception ex) {
+            System.out.println("Exception: " + ex.getMessage());
+        }
         System.out.println(conta.getSaldo());
     }
 }
