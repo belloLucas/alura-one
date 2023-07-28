b6630f5516b550c3868ff2f955007e0a00abcab7
belloLucas <lucasbellodev@gmail.com>
2023-07-28 10:49:24 -0300

    Aula sobre string args concluida

---
+++ alura-polimorfismo/bytebank-herdado-conta/src/br/com/bytebank/banco/tests/TesteArrayString.java
@@ -0,0 +1,11 @@
+package br.com.bytebank.banco.tests;
+
+public class TesteArrayString {
+    public static void main(String[] args) {
+        System.out.println("It works!");
+
+        for(int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+    }
+}
