65df11f7963465151591df393c44a72b6fad8e27
belloLucas <lucasbellodev@gmail.com>
2023-08-18 16:08:13 -0300

    Testando INSERT via execute()

---
+++ aula-java-e-jdbc/loja-virtual-repository/src/TestaInsercao.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+    public static void main(String[] args) throws SQLException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection con = connectionFactory.recuperarConexao();
+
+        Statement stm = con.createStatement();
+        stm.execute("INSERT INTO PRODUTO(nome, descricao) VALUES ('Mouse', 'Mouse sem frio')", Statement.RETURN_GENERATED_KEYS);
+        ResultSet set = stm.getGeneratedKeys();
+
+        while(set.next()){
+            Integer id = set.getInt(1);
+            System.out.println(id);
+        }
+
+        con.close();
+    }
+}
