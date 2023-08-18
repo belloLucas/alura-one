9f1dd7469b8b15d52c7164a8e2c7f416b1ec6268
belloLucas <lucasbellodev@gmail.com>
2023-08-18 10:34:18 -0300

    feat: Refactoring code. Isolating the connection with the db in a separate class

--- aula-java-e-jdbc/loja-virtual-repository/src/TestaListagem.java
+++ aula-java-e-jdbc/loja-virtual-repository/src/TestaListagem.java
@@ -1,20 +1,24 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestaListagem {
     public static void main(String[] args) throws SQLException {
-        Connection con = DriverManager
-                .getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-                        "root", "root");
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection con = connectionFactory.recuperarConexao();
 
         Statement stm = con.createStatement();
         stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
         ResultSet rst = stm.getResultSet();
+
         while(rst.next()){
             Integer id = rst.getInt("ID");
             String nome = rst.getString("NOME");
             String descricao = rst.getString("DESCRICAO");
             System.out.println(id + " | " + nome + " | " + descricao);
         }
+
         con.close();
     }
 }
