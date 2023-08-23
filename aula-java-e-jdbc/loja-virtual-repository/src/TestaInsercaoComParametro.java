dc942c08f0c1329829d51ecb2d137f253fccc629
belloLucas <lucasbellodev@gmail.com>
2023-08-23 11:09:21 -0300

    Aplicando try-with-resources

--- aula-java-e-jdbc/loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ aula-java-e-jdbc/loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -3,27 +3,23 @@ import java.sql.*;
 public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        Connection con = connectionFactory.recuperarConexao();
+        try(Connection con = connectionFactory.recuperarConexao()) {
+            con.setAutoCommit(false);
 
-        con.setAutoCommit(false);
-
-        try{
-            PreparedStatement stm = con.prepareStatement(
+            try (PreparedStatement stm = con.prepareStatement(
                     "INSERT INTO PRODUTO(nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+            ) {
+                adicionarVariavel("SmartTV", "45 polegadas", stm);
+                adicionarVariavel("Radio", "Radio de bateria", stm);
 
-            adicionarVariavel("SmartTV", "45 polegadas", stm);
-            adicionarVariavel("Radio", "Radio de bateria", stm);
-
-            con.commit();
-
-            stm.close();
-            con.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println("Rollback foi executado: " + ex);
-            con.rollback();
+                con.commit();
+            } catch (
+                    Exception ex) {
+                ex.printStackTrace();
+                System.out.println("Rollback foi executado: " + ex);
+                con.rollback();
+            }
         }
-
     }
 
     private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
@@ -35,13 +31,13 @@ public class TestaInsercaoComParametro {
         }
 
         stm.execute();
-        ResultSet set = stm.getGeneratedKeys();
 
-        while(set.next()){
-            Integer id = set.getInt(1);
-            System.out.println(id);
-        }
+        try(ResultSet set = stm.getGeneratedKeys()){
+            while(set.next()){
+                Integer id = set.getInt(1);
+                System.out.println(id);
+            }
+        };
 
-        set.close();
     }
 }
