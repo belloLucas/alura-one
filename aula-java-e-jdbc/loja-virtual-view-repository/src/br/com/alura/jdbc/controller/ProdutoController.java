eff987b221cfa0a73dfac53c4f046a81c528ee25
belloLucas <lucasbellodev@gmail.com>
2023-08-29 11:50:38 -0300

    feat: ProdutoController finalizado, todas as operações de CRUD agora estão funcionais

--- aula-java-e-jdbc/loja-virtual-view-repository/src/br/com/alura/jdbc/controller/ProdutoController.java
+++ aula-java-e-jdbc/loja-virtual-view-repository/src/br/com/alura/jdbc/controller/ProdutoController.java
@@ -1,17 +1,29 @@
 package br.com.alura.jdbc.controller;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.alura.jdbc.dao.ProdutoDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Produto;
 
 public class ProdutoController {
 
+	private ProdutoDAO produtoDAO;
+
+	public ProdutoController() {
+		Connection con = new ConnectionFactory().recuperarConexao();
+		this.produtoDAO = new ProdutoDAO(con);
+	}
+
 	public void deletar(Integer id) {
+		this.produtoDAO.deletar(id);
 		System.out.println("Deletando produto");
 	}
 
 	public void salvar(Produto produto) {
+		this.produtoDAO.salvar(produto);
 		System.out.println("Salvando produto");
 	}
 
@@ -19,10 +31,11 @@ public class ProdutoController {
 		List<Produto> produtos = new ArrayList<Produto>();
 		produtos.add(new Produto("Nome do Produto de teste"
 				, "Descrição do produto de teste"));
-		return produtos;
+		return this.produtoDAO.listar();
 	}
 
 	public void alterar(String nome, String descricao, Integer id) {
+		this.produtoDAO.alterar(nome, descricao, id);
 		System.out.println("Alterando produto");
 	}
 }
