022ae6c7bc162a208253c6b120e653c13b53c714
belloLucas <lucasbellodev@gmail.com>
2023-09-09 12:49:17 -0300

    feat: Criando método atualizar() que serve para garantir que a entidade esteja no estado MANAGED

--- alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
+++ alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/dao/CategoriaDAO.java
@@ -14,4 +14,8 @@ public class CategoriaDAO {
     public void cadastrar(Categoria categoria) {
         this.em.persist(categoria);
     }
+
+    public void atualizar(Categoria categoria) {
+        this.em.merge(categoria);
+    }
 }
