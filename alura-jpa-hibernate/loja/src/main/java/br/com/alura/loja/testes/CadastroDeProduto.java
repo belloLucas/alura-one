046140e295654a6d476e5990e9ee858cc1b8c6b5
belloLucas <lucasbellodev@gmail.com>
2023-09-09 12:50:20 -0300

    Testando merge, para retornar uma entidade DETACHED para MANAGED

--- alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,27 +1,25 @@
 package br.com.alura.loja.testes;
 
-import br.com.alura.loja.dao.CategoriaDAO;
-import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
-import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import java.math.BigDecimal;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
         Categoria celulares = new Categoria("CELULARES");
-        Produto celular = new Produto("Xiaomi Redmi", "Smartphone", new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDAO produtoDAO = new ProdutoDAO(em);
-        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
-
         em.getTransaction().begin();
-        categoriaDAO.cadastrar(celulares);
-        produtoDAO.cadastrar(celular);
-        em.getTransaction().commit();
-        em.close();
+
+        em.persist(celulares);
+        celulares.setNome("XPTO");
+
+        em.flush();
+        em.clear();
+
+        celulares = em.merge(celulares);
+        celulares.setNome("1234");
+        em.flush();
     }
 }
