789e9069f2ad535b6481acfe778844a3d0605957
belloLucas <lucasbellodev@gmail.com>
2023-09-11 10:35:19 -0300

    feat: Encapsulando códigos do método principal em um método a parte e testando utilização de SELECTS através dos métodos criados no DAO

--- alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,28 +1,42 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProduto {
     public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+
+        Produto p = produtoDAO.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDAO.buscarTodos();
+        todos.forEach(p2 -> System.out.println(p2.getNome()));
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "128GB", new BigDecimal(800), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+
         em.getTransaction().begin();
 
-        em.persist(celulares);
-        celulares.setNome("XPTO");
+        categoriaDAO.cadastrar(celulares);
+        produtoDAO.cadastrar(celular);
 
-        em.flush();
-        em.clear();
-
-        celulares = em.merge(celulares);
-        celulares.setNome("1234");
-        em.flush();
-
-        em.remove(celulares);
-        em.flush();
+        em.getTransaction().commit();
+        em.close();
     }
 }
