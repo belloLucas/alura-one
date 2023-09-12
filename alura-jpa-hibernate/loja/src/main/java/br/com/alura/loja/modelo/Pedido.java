31a79c57dce5240546972597b311125997a8093e
belloLucas <lucasbellodev@gmail.com>
2023-09-12 10:01:56 -0300

    feat: Criando e já inicializando uma ArrayList que guardará os itens de pedido. Criando também o método adicionarItem que tem o comportamento de adicionar o item na entidade ItemPedido e também adiciona esse item na ArrayList itens da propria entidade Pedido

--- alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -3,6 +3,8 @@ package br.com.alura.loja.modelo;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "pedidos")
@@ -16,10 +18,18 @@ public class Pedido {
     @ManyToOne
     private Cliente cliente;
 
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens = new ArrayList<>();
+
     public Pedido() {}
 
     public Pedido(Cliente cliente) {
         this.cliente = cliente;
+    }
+
+    public void adicionarItem(ItemPedido item) {
+        item.setPedido(this);
+        this.itens.add(item);
     }
 
     public Long getId() {
