27f88c247ebeac0274e55a197e54be53e4e90b0f
belloLucas <lucasbellodev@gmail.com>
2023-09-13 10:41:52 -0300

    feat: Criando classe para testar o pedido

---
+++ alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -0,0 +1,54 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.CategoriaDAO;
+import br.com.alura.loja.dao.ClienteDAO;
+import br.com.alura.loja.dao.PedidoDAO;
+import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.*;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+public class CadastroDePedido {
+    public static void main(String[] args) {
+        popularBancoDeDados();
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        ClienteDAO clienteDAO = new ClienteDAO(em);
+
+        Produto produto = produtoDAO.buscarPorId(1l);
+        Cliente cliente = clienteDAO.buscarPorId(1l);
+
+        em.getTransaction().begin();
+
+        Pedido pedido = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+
+        PedidoDAO pedidoDAO = new PedidoDAO(em);
+        pedidoDAO.cadastrar(pedido);
+
+        em.getTransaction().commit();
+    }
+
+    private static void popularBancoDeDados() {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "128GB", new BigDecimal(800), celulares);
+        Cliente cliente = new Cliente("Lucas", "12345678910");
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+        ClienteDAO clienteDAO = new ClienteDAO(em);
+
+        em.getTransaction().begin();
+
+        categoriaDAO.cadastrar(celulares);
+        produtoDAO.cadastrar(celular);
+        clienteDAO.cadastrar(cliente);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
