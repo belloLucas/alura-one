c3f565b76da1d97ffc33ae16ca86242521fabc21
belloLucas <lucasbellodev@gmail.com>
2023-09-20 10:55:09 -0300

    feat: Criando método valorTotalVendido que executa a query para realizar a consulta e soma no banco de dados.

--- alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/dao/PedidoDAO.java
+++ alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/dao/PedidoDAO.java
@@ -1,11 +1,9 @@
 package br.com.alura.loja.dao;
 
 import br.com.alura.loja.modelo.Pedido;
-import br.com.alura.loja.modelo.Produto;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
-import java.util.List;
 
 public class PedidoDAO {
     private EntityManager em;
@@ -16,5 +14,10 @@ public class PedidoDAO {
 
     public void cadastrar(Pedido pedido) {
         this.em.persist(pedido);
+    }
+
+    public BigDecimal valorTotalVendido() {
+        String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
+        return em.createQuery(jpql, BigDecimal.class).getSingleResult();
     }
 }
