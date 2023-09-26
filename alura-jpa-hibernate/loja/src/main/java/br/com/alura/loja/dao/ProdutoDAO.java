3320208ff707ac9412f385be6d74ddc1331c0840
belloLucas <lucasbellodev@gmail.com>
2023-09-26 12:34:37 -0300

    feat: Utilizando query com parâmetros dinâmicos

--- alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
+++ alura-jpa-hibernate/loja/src/main/java/br/com/alura/loja/dao/ProdutoDAO.java
@@ -61,13 +61,13 @@ public class ProdutoDAO {
     public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro) {
         String jpql = "SELECT p FROM Produto p WHERE 1=1 ";
         if(nome != null && !nome.trim().isEmpty()){
-            jpql = "AND p.nome = :nome";
+            jpql = " AND p.nome = :nome ";
         }
         if(preco != null){
-            jpql = "AND p.preco = :preco";
+            jpql = " AND p.preco = :preco ";
         }
         if(dataCadastro != null){
-            jpql = "AND p.dataCadastro = :dataCadastro";
+            jpql = " AND p.dataCadastro = :dataCadastro ";
         }
 
         TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
