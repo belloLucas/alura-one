328fcb858509692d6419fd968597519486c516e0
belloLucas <lucasbellodev@gmail.com>
2023-09-29 15:35:18 -0300

    feat: Adicionando construtor para converter DTO e salvar seus valores nos atributos da entidade Paciente

--- alura-spring-boot-3-desenvolva-uma-api-rest/api/src/main/java/med/voll/api/paciente/Paciente.java
+++ alura-spring-boot-3-desenvolva-uma-api-rest/api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -16,6 +16,7 @@ import med.voll.api.endereco.Endereco;
 public class Paciente {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
     private String telefone;
@@ -23,4 +24,12 @@ public class Paciente {
 
     @Embedded
     private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
