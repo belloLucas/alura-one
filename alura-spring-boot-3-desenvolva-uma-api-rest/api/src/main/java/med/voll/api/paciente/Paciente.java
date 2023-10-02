d4830d277f0067a472b922b59f9d05a9dfe2f32b
belloLucas <lucasbellodev@gmail.com>
2023-10-02 17:50:59 -0300

    feat: Criando atributo ativo e definindo ele como true no construtor. Criando também método excluir() que quando chamado, define esse paciente como inativo

--- alura-spring-boot-3-desenvolva-uma-api-rest/api/src/main/java/med/voll/api/paciente/Paciente.java
+++ alura-spring-boot-3-desenvolva-uma-api-rest/api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -25,7 +25,10 @@ public class Paciente {
     @Embedded
     private Endereco endereco;
 
+    private boolean ativo;
+
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -37,5 +40,9 @@ public class Paciente {
         if(dados.nome() != null) this.nome = dados.nome();
         if(dados.telefone() != null) this.telefone = dados.telefone();
         if(dados.endereco() != null) this.endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
